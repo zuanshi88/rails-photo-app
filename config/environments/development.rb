913c8f7feaed4a9a1d2730c413c5ddd6799dbc03
zuanshi88 <adwhitmer@gmail.com>
2022-10-06 11:23:59 -0500

    adding email verification

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,10 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  #switch to :smtp to sent mail 
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { :host => 'http://127.0.0.1'}
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
